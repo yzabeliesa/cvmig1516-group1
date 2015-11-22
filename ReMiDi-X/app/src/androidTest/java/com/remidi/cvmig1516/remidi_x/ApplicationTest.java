6ac902238913966b4c6e705fdb0ff86091083502
yzabeliesa <abbeymq@gmail.com>
2015-11-22 20:26:10 +0800

    Add new ReMiDi Android files

---
+++ ReMiDi-X/app/src/androidTest/java/com/remidi/cvmig1516/remidi_x/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.remidi.cvmig1516.remidi_x;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
